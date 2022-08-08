d30e42d3050cb5921d5f1d58cf6fb930282b4f2c
Sage Skaff <skaffsage@gmail.com>
2022-08-08 09:12:20 -0700

    Add remove plant link

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :plots, only: [:index]
+
+  resources :plant_plots, only: [:destroy]
 end
--- spec/features/plots/index_spec.rb
+++ spec/features/plots/index_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe 'plots index page' do
     end
   end
 
-  it 'lists all plot numbers' do
+  it 'lists names of plots plants' do
     garden = Garden.create!(name: 'Turing Community Garden', organic: true)
     plot1 = garden.plots.create!(number: 1, size: 'Large', direction: 'East')
     plot2 = garden.plots.create!(number: 2, size: 'Medium', direction: 'West')
@@ -59,6 +59,47 @@ RSpec.describe 'plots index page' do
     within "#plot-#{plot3.id}" do
       expect(page).to have_content('Sage')
       expect(page).to_not have_content('Basil')
+      expect(page).to_not have_content('Tomato')
+    end
+  end
+
+  it 'has link to remove plant next to each plants name' do
+    garden = Garden.create!(name: 'Turing Community Garden', organic: true)
+    plot1 = garden.plots.create!(number: 1, size: 'Large', direction: 'East')
+    plot2 = garden.plots.create!(number: 2, size: 'Medium', direction: 'West')
+    plot3 = garden.plots.create!(number: 3, size: 'Small', direction: 'North')
+
+    plant1 = Plant.create!(name: 'Sage', description: 'Best plant', days_to_harvest: 70)
+    plant2 = Plant.create!(name: 'Basil', description: 'Good plant', days_to_harvest: 80)
+    plant3 = Plant.create!(name: 'Tomato', description: 'Versatile plant', days_to_harvest: 90)
+
+    pp1 = PlantPlot.create!(plant: plant1, plot: plot1)
+    pp2 = PlantPlot.create!(plant: plant3, plot: plot1)
+    pp3 = PlantPlot.create!(plant: plant1, plot: plot2)
+    pp4 = PlantPlot.create!(plant: plant2, plot: plot2)
+    pp5 = PlantPlot.create!(plant: plant3, plot: plot2)
+    pp6 = PlantPlot.create!(plant: plant1, plot: plot3)
+
+    visit '/plots'
+
+    within "#plot-#{plot1.id}" do
+      within "#plant-#{pp1.id}" do
+        expect(page).to have_link('Remove Plant')
+      end
+
+      within "#plant-#{pp2.id}" do
+        expect(page).to have_content('Tomato')
+        expect(page).to have_link('Remove Plant')
+        click_link 'Remove Plant'
+      end
+    end
+
+    expect(current_path).to eq('/plots')
+
+    within "#plot-#{plot1.id}" do
+      within "#plant-#{pp1.id}" do
+        expect(page).to have_link('Remove Plant')
+      end
       expect(page).to_not have_content('Tomato')
     end
   end
