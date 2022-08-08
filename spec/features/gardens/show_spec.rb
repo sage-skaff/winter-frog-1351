fedd73f4fa960bba8b9c4edca364f0124c439832
Sage Skaff <skaffsage@gmail.com>
2022-08-08 09:25:19 -0700

    Test: garden show page plant list

---
+++ spec/features/gardens/show_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'gardens show page' do
+  it 'lists unique plants with less than 100 days to harvest in this gardens plots' do
+    garden = Garden.create!(name: 'Turing Community Garden', organic: true)
+    plot1 = garden.plots.create!(number: 1, size: 'Large', direction: 'East')
+    plot2 = garden.plots.create!(number: 2, size: 'Medium', direction: 'West')
+    plot3 = garden.plots.create!(number: 3, size: 'Small', direction: 'North')
+
+    plant1 = Plant.create!(name: 'Sage', description: 'Best plant', days_to_harvest: 70)
+    plant2 = Plant.create!(name: 'Basil', description: 'Good plant', days_to_harvest: 80)
+    plant3 = Plant.create!(name: 'Tomato', description: 'Versatile plant', days_to_harvest: 101)
+    plant4 = Plant.create!(name: 'Lime', description: 'Sour plant', days_to_harvest: 90)
+
+    PlantPlot.create!(plant: plant1, plot: plot1)
+    PlantPlot.create!(plant: plant3, plot: plot1)
+    PlantPlot.create!(plant: plant1, plot: plot2)
+    PlantPlot.create!(plant: plant2, plot: plot2)
+    PlantPlot.create!(plant: plant3, plot: plot2)
+    PlantPlot.create!(plant: plant1, plot: plot3)
+
+    visit "/gardens/#{garden.id}"
+
+    within '#plants' do
+      expect(page).to have_content('Sage')
+      expect(page).to have_content('Basil')
+      expect(page).to_not have_content('Tomato')
+      expect(page).to_not have_content('Lime')
+    end
+  end
+end
